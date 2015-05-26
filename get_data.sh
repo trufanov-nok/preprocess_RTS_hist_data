00b84ac1b373875d72adfd00d1fd110ef92e3ef3
Alexander Trufanov <trufanovan@gmail.com>
2015-05-26 16:44:07 +0400

    better index.html filtration

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-wget -nH -np --cut-dirs=5 -r http://ftp.micex.com/pub/info/stats/history/F/$1
+wget -nH -np --cut-dirs=5 -R index.html -r http://ftp.micex.com/pub/info/stats/history/F/$1
 
-if [ -f "index.html" ]; then
-rm index.html
-fi
+#if [ -f "index.html" ]; then
+#rm index.html
+#fi
 
